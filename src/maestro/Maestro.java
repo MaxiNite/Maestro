aa5a42b346173a0f5b57faa7a62487bc5215688c
Daniel Holguin <39926374+MaxiNite@users.noreply.github.com>
2021-02-20 18:02:49 -0700

    Creacion de sistema Maestro

---
+++ src/maestro/Maestro.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maestro;
+
+/**
+ *
+ * @author Dhtey
+ */
+public class Maestro {
+    private String nombre, apellidos, materia;
+    private int numero, id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Maestro{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", materia=" + materia + ", numero=" + numero + ", id=" + id + '}';
+    }
+    //sdsd
+}
